5f41756ec0d1530bf9194d16f2ba78c820f9b381
Abdurrahman PÜLAT <57863133+AbdurrahmanPulat@users.noreply.github.com>
2022-04-27 12:42:41 +0300

    Add files via upload

---
+++ src/Form.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+ class Form extends Component {
+   constructor()
+   {
+      super();
+      this.onChange=this.onChange.bind(this);
+      this.onSubmit=this.onSubmit.bind(this);
+
+   }
+   state=
+   {
+     name:'',
+     phone:''
+   };
+   
+   onChange(e)
+   {
+     this.setState(
+       {
+         [e.target.name]:e.target.value
+       }
+     )
+   }
+  onSubmit(e)
+	{
+		e.preventDefault();
+		this.props.addContact(
+			{
+				...this.state
+			}
+		);
+		this.setState({
+			name:'',
+			phone:''
+		});
+	}
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+        <input name="name" id ="name" onChange={this.onChange} value={this.state.name} placeholder='Bir İsim Giriniz' />
+        <br />
+        <input name="phone" id ="phone" onChange={this.onChange} value={this.state.phone} placeholder='Bir Numara Giriniz' />
+        <button>Ekle</button>
+
+        </form>
+      </div>
+    )
+  }
+}
+export default Form
